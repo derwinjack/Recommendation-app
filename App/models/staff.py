7b6ed840bffdd6f0d5e5c0b0da42ecbde7b0d21a
divvsss <99010526+divvsss@users.noreply.github.com>
2022-10-04 13:39:34 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -2,5 +2,5 @@ from App.database import db
 from App.models import User
 
 class Staff(User):
-    # staffID = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    
      notificationFeed = db.relationship('NotificationFeed', backref=db.backref('staff', lazy='joined'))
