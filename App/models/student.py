5478c3a0e915a7b49a498fac4d3067dc51e0cb40
divvsss <99010526+divvsss@users.noreply.github.com>
2022-10-04 13:40:17 -0400

    Update student.py

--- App/models/student.py
+++ App/models/student.py
@@ -2,6 +2,5 @@ from App.database import db
 from App.models import User
 
 class Student(User):
-    # studentID = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     recommendationListing = db.relationship('RecommendationListing', backref=db.backref('student', lazy='joined'))
     
