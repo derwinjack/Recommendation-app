ac21714e3c8e5ed1efe62286d710bb249434bd37
divvsss <99010526+divvsss@users.noreply.github.com>
2022-10-04 09:20:11 -0400

    Update recommendationListing.py

--- App/models/recommendationListing.py
+++ App/models/recommendationListing.py
@@ -3,7 +3,3 @@ class recommendationListing(db.model):
 rid = db.Column(db.Integer, primary_key=True) 
 studentidrec = db.Column(db.Integer, foreign_key=True)
 
-
-def __init__(self, rid, studentidrec):
-  self.rid = rid
-  self.studentidrec = studentidrec
