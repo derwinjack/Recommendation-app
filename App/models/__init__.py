38dab557a599f3aa19ea8e925ef60ed48dc7b825
Rehannah <rehannah12345@gmail.com>
2022-10-04 11:34:53 -0400

    changes

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import *
 from .student import *
-# from .staff import *
+from .staff import *
 from .recommendation import *
 from .recommendationListing import *
-# from .notification import *
-# from .notificationFeed import *
+from .notification import *
+from .notificationFeed import *
