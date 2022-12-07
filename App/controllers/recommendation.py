c3d6e59176beed4a52f92802ab093fd252d1b42c
derwinjack <62758674+derwinjack@users.noreply.github.com>
2022-12-07 10:13:25 -0400

    Update recommendation.py

--- App/controllers/recommendation.py
+++ App/controllers/recommendation.py
@@ -2,20 +2,20 @@ from App.models import Recommendation, Student
 from App.database import db
 from sqlalchemy.exc import IntegrityError
 
-def create_recommendation(sentFromStaffID, sentToStudentID, recURL):
-    newrec = Recommendation(sentFromStaffID=sentFromStaffID, sentToStudentID=sentToStudentID, recURL=recURL)
+def create_recommendation(staffID, studentID, recomText):
+    newrec = Recommendation(staffID=staffID, studentID=studentID, recomText=recomText)
     return newrec
 
-def send_recommendation(sentFromStaffID, sentToStudentID, recURL):
-    student = Student.query.get(sentToStudentID)
-    newrec = create_recommendation(sentFromStaffID, sentToStudentID, recURL)
+def send_recommendation(staffID, studentID, recURL):
+    student = Student.query.get(studentID)
+    newrec = create_recommendation(staffID, studentID, recURL)
     try:
         db.session.add(newrec)
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
         return None
-    student.recommendationList.append(newrec)
+    student.recomlist.append(newrec)
     try:
         db.session.add(student)
         db.session.commit()
@@ -26,6 +26,9 @@ def send_recommendation(sentFromStaffID, sentToStudentID, recURL):
 
 def get_all_recommendations():
     return Recommendation.query.all()
+    
+def get_student_recommendations(studentID):
+    return Recommendation.query.filter_by(studentID=studentID).all()
 
 def get_all_recommendations_json():
     recs = get_all_recommendations()
@@ -35,10 +38,14 @@ def get_all_recommendations_json():
     return recs
 
 def get_recommendation(studentID, recID):
-    rec = Recommendation.query.filter_by(sentToStudentID=studentID, recID=recID).first()
+    rec = Recommendation.query.filter_by(studentID=studentID, recomID=recID).first()
     if rec:
         return rec.toJSON()
     return None
+
+
+
+
 
 
 
